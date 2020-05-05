e11a19d56cd3fbe74b1e781103e8b7b2d3c782a5
Yash Djson Dookun <dookun88@gmail.com>
2020-05-05 09:40:39 +0400

    Fixed readme

--- repo-hub.py
+++ repo-hub.py
@@ -2,18 +2,27 @@ import os
 
 current_directory = (os.path.basename(os.getcwd()))
 
+
 def create_repository():
-    os.system(f'echo # {current_directory} >> README.md')
-    os.system("git init .")
-    os.system("git add .")
-    os.system('git commit -m "Initial Commit"')
-    os.system("hub create")
-    os.system("git push origin master")
+    try:
+        os.system(f'echo # {current_directory} >> README.md')
+        os.system("git init .")
+        os.system("git add .")
+        os.system('git commit -m "Initial Commit"')
+        os.system("hub create")
+        os.system("git push origin master")
+    except:
+        print("Oops..Something Went Wrong")
+
 
 def push_to_repository(commitArg):
-    os.system("git add .")
-    os.system(f'git commit -m "{commitArg}"')
-    os.system("git push origin master")
+    try:
+        os.system("git add .")
+        os.system(f'git commit -m "{commitArg}"')
+        os.system("git push origin master")
+    except:
+        os.system("git status")
+
 
 # create_repository()
 push_to_repository("Fixed readme")
